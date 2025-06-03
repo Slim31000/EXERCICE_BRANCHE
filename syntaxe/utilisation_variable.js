029f93831fc5937e72d4f3a4eaf080c766895f7d
slim31000 <abbsalem31@live.com>
2025-06-03 11:04:50 +0200

    add exercice variable

---
+++ syntaxe/utilisation_variable.js
@@ -0,0 +1,10 @@
+let nom="abibsi";
+let prenom="bensalem";
+console.log(`bonjour ${prenom} ${nom}`);
+
+const age= 64;
+my_age=38;
+
+let age_restant= age - my_age;
+
+console.log(`${age_restant} années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! `)
